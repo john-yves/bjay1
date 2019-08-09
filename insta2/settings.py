e84416e056b141f2d3e7353e152a63eea96d3490
Yves Mugisha <mugishayves74@gmail.com>
2019-08-10 00:49:33 +0200

    again

--- insta2/settings.py
+++ insta2/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
